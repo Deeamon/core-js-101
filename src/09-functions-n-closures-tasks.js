f1246a51c2bd62bf125faffb4a836f149870a6bf
Deeamon <marinasugutova@gmail.com>
2019-12-22 19:11:09 +0300

    add part of solutions to the part 9

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -62,8 +62,9 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...arg) {
+  const arh = [...arg].reverse();
+  return (x) => arh.map((el, i) => el * x ** i).reduce((acc, el) => acc + el);
 }
 
 
@@ -169,8 +170,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let temp = startFrom - 1;
+  return () => {
+    temp += 1;
+    return temp;
+  };
 }
 
 
